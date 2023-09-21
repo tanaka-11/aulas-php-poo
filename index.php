5cb002d837544f416e5c21f6592f75fcbe2d337e
Marina Tanaka <huttenloucher@gmail.com>
2023-09-21 11:14:07 -0300

    Criado pagina index para o exemplo de PHP POO

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Exemplo - PHP POO</title>
+
+</head>
+<body>
+    <h1>PHP com POO(Orientação a Objetos)</h1>
+    <hr>
+    
+</body>
+</html>
