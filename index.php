e776830496c8f119c4f548fa35f35b2215192380
Marina Tanaka <huttenloucher@gmail.com>
2023-09-22 09:12:39 -0300

    Exemplo de __construct e this

--- index.php
+++ index.php
@@ -5,25 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Exemplo 02 - PHP POO</title>
+    <title>Exemplo 03 - PHP POO</title>
 
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 02</h1>
+    <h1>PHP com POO - Exemplo 03</h1>
     <hr>
 
     <h2>Assuntos abordados:</h2>
     
     <ul>
-       
+       <li>Método construtor (__construct) com atribuição obrigatória de valores aos parâmetros do objeto</li>
+       <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto</li>
     </ul>
 
 <?php 
     // Classe
-    require_once 'src/Cliente.php';
+        require_once 'src/Cliente.php';
+
+    // Exemplo de criação de objeto com o construtor, obrigatoriamente temos que passar os parâmetros que atribuimos na classe.
+        $A = new Cliente("Marina Tanaka", "mtanakasg@gmail.com");
+        $B = new Cliente("Beatriz Kogici", "umemail@gmail.com");
+
+    // Como passamos apenas nome e email no construct, o restante dos dados continuamos a chamar com a mesma sintaxe anterior.
+        $A->telefones = ['11 98681-7759'];
 ?>
   
-  
+
+
+
+
+  <!-- Teste -->
+  <pre><?=var_dump($A, $B)?></pre>
+
 </body>
 
 </html>
