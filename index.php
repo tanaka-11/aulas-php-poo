bf8e4f67457b6a3b0daa8c76a2ad6f56c0bbe058
Marina Tanaka <huttenloucher@gmail.com>
2023-09-26 09:25:50 -0300

    Atribuindo valores utilizando a superclasse e nossa subclasse

--- index.php
+++ index.php
@@ -24,8 +24,20 @@
 require_once 'src/PessoaFisica.php';
 
 // Criação do objeto.
-$clientePF = new PessoaFisica
+$clientePF = new PessoaFisica;
 
+// Colocando um nome no Cliente(PF)
+$clientePF->setNome("Marina Tanaka");
+// $clientePF->nome = "Marina Tanaka"; (Está linha inves de adicionar o nome nos dados ja existente ele cria uma nova propriedade com o valor passado). NÃO UTILIZAR POIS É UM ERRO NO PROPRIO PHP.
+
+//Colocando um email no Cliente(PF)
+$clientePF->setEmail("mtanakasg@gmail.com");
+
+// Colocando idade (PF)
+$clientePF->setIdade(21);
+
+// Colocando cpf (PF)
+$clientePF->setCpf("503-077-318-31");
 ?>
 
     <!-- Teste -->
