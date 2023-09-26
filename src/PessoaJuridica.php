f608be679bba97cedca6a60d8bb3576e7d48c5f0
Marina Tanaka <huttenloucher@gmail.com>
2023-09-26 09:43:58 -0300

    Criado a class pessoa juridica e passado seus getters e setters

---
+++ src/PessoaJuridica.php
@@ -0,0 +1,49 @@
+<?php
+// Importando a super classe "Cliente"
+require_once 'Cliente.php';
+
+// Criando a sub classe "Pessoa Juridica" utilizando a palavra chave "extends"
+class PessoaJuridica extends Cliente {
+    private int $anoFundacao;
+    private string $cnpj;
+    private string $nomeFantasia;
+
+    // Getter e Setter via Extensão (Ano Fundação)
+    public function getAnoFundacao(): int
+    {
+        return $this->anoFundacao;
+    }
+
+    public function setAnoFundacao(int $anoFundacao): self
+    {
+        $this->anoFundacao = $anoFundacao;
+
+        return $this;
+    }
+
+    // Getter e Setter via Extensão (CNPJ)
+    public function getCnpj(): string
+    {
+        return $this->cnpj;
+    }
+
+    public function setCnpj(string $cnpj): self
+    {
+        $this->cnpj = $cnpj;
+
+        return $this;
+    }
+
+    // Getter e Setter via Extensão (Nome Fantasia)
+    public function getNomeFantasia(): string
+    {
+        return $this->nomeFantasia;
+    }
+
+    public function setNomeFantasia(string $nomeFantasia): self
+    {
+        $this->nomeFantasia = $nomeFantasia;
+
+        return $this;
+    }
+}
