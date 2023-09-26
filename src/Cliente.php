5483d0d592c7f9a92dce5b00b1f6b02d81928f84
Marina Tanaka <huttenloucher@gmail.com>
2023-09-26 07:58:24 -0300

    update semantico

--- src/Cliente.php
+++ src/Cliente.php
@@ -3,7 +3,7 @@
   
 // *Obs. Classes SEMPRE começam com a letra maiuscula.
 class Cliente {
-    // - Criando Propriedades(Variaveis) privadas o que significa que não é possivel alterar dados fora da classe -
+    // - Criando Propriedades(Variaveis) privadas, o que significa que não é possivel alterar dados fora da classe -
     private string $nome;
     private string $email;
     private string $senha;
